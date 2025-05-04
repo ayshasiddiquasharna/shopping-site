e48a0f8c73f50a84f79ab922f7614054ce35b215
Aysha Siddiqua Sharna <112265686+ayshasiddiquasharna@users.noreply.github.com>
2025-05-04 21:18:16 +0600

    Delete shop-now.js

--- shop-now.js
+++
@@ -1,7 +0,0 @@
-function navigateToProducts() {
-    // You can include additional logic or data here
-    var searchProducts = "your_search_value";  // Replace this with the actual search value
-
-    // Navigate to results.html with search parameter
-    window.location.href = `products.html?search=${encodeURIComponent(searchProducts)}`;
-}
